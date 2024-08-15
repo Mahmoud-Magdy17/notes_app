17fc1e3714701fde732b408998828a34e1695b69
Mahmoud-Magdy17 <MahmoudMagdy176203@gmail.com>
2024-08-15 11:33:48 +0300

    build add note function

--- lib/features/add_note_feature/logic/add_note_cubit/add_note_cubit.dart
+++ lib/features/add_note_feature/logic/add_note_cubit/add_note_cubit.dart
@@ -1,7 +1,20 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notes_app/core/constants/constants.dart';
+import 'package:notes_app/core/models/note_model.dart';
 
 import 'add_note_states.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
+  addNote(NoteModel note) async {
+    emit(AddNoteLoading());
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      notesBox.add(note);
+      emit(AddNoteSuccess());
+    } catch (e) {
+      emit(AddNoteError());
+    }
+  }
 }
