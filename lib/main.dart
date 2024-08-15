e589025e0aa7e7f928f536c02a6fa845a610790a
Mahmoud-Magdy17 <MahmoudMagdy176203@gmail.com>
2024-08-15 12:16:17 +0300

    add bloc observer and progress hub

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,21 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notes_app/core/models/note_model.dart';
+import 'package:notes_app/simple_bloc_observer.dart';
 import 'core/constants/constants.dart';
 import 'features/all_notes_features/ui/home_view.dart';
 
 void main() async {
+  Bloc.observer = SimpleBlocObserver();
   await Hive.initFlutter();
-  await Hive.openBox(kNotesBox);
+  await Hive.openBox<NoteModel>(kNotesBox);
   runApp(const NotesApp());
 }
 
 class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
